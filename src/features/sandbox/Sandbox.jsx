c25a77a88ff2afe543ff4492daa5ab01bfd311dc
Sam Liu <o4d0b@outlook.com>
2021-10-30 00:02:26 -0700

    Added Google Map section for displaying event location

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { openModal } from '../../app/common/modals/modalReducer';
+import TestPlaceInput from './TestPlaceInput';
+import TestMap from './TestMap';
 import { decrement, increment } from './testReducer';
 
 export default function Sandbox() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.test.data);
+
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  const [location, setLocation] = useState(defaultProps);
+
+  function handleSelectLocation(latLng) {
+    setLocation({ ...location, center: { lat: latLng.lat, lng: latLng.lng } });
+  }
 
   return (
     <>
@@ -29,6 +44,10 @@ export default function Sandbox() {
         content="Open Modal"
         color="teal"
       />
+      <div style={{ marginTop: 15 }}>
+        <TestPlaceInput setLocation={handleSelectLocation} />
+        <TestMap location={location} />
+      </div>
     </>
   );
 }
